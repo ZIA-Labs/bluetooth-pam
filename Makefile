723042007e90b2de4445ff3172931efee9834821
Neil Klingensmith <neil@cs.luc.edu>
2023-02-13 22:54:31 -0600

    PAM module reads BLE dev ID from ~/.zia/devices file. sudo mkdir -p /opt/zialabs/usr/lib/pam sudo cp zia_auth.so /opt/zialabs/usr/lib/pam does simplified installation.

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
 
-
+ROOTDISK := /dev/disk3s1
 
 mac:
 	gcc -dynamiclib -fPIC -fno-stack-protector src/zia_auth.c -o zia_auth.so -lpam
+
+macinstall:
+	sudo mkdir -p /opt/zialabs/usr/lib/pam
+	sudo cp zia_auth.so /opt/zialabs/usr/lib/pam
+#	sudo mount -o nobrowse -t apfs $(ROOTDISK) ~/mount
+#	sudo cp zia_auth.so ~/mount/usr/lib/pam
+#	$(shell sudo bless --mount $HOME/mount/System/Library/CoreServices/ --setBoot --create-snapshot)
+#	sudo umount ~/mount
--- src/zia_auth.c
+++ src/zia_auth.c
@@ -1,3 +1,14 @@
+/*
+ * zia_auth.c
+ *
+ * Neil Klingensmith
+ * 2-13-2023
+ *
+ * Copyright 2023 ZIA Labs
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,37 +16,103 @@
 #include <security/pam_modules.h>
 #include <unistd.h>
 
+
+/*
+ * check_device_connected
+ *
+ * Calls blueutil to find out if a particular device is connected. Returns
+ * PAM_SUCCESS if device is connected, otherwise returns PAM_AUTH_ERR.
+ *
+ */
+static int check_device_connected(char *devid) {
+  // Run `blueutil --is-connected` to find out if neil's headphones are connected
+  FILE *fp;
+  char path[1035];
+
+  /* Open the command for reading. */
+  fp = popen("BLUEUTIL_ALLOW_ROOT=1 blueutil --is-connected  08-ff-44-07-92-4e", "r");
+  if (fp == NULL) {
+    printf("ZIA PAM Module: Failed to run blueutil\n");
+    return PAM_AUTH_ERR;
+  }
+
+  /* Read the output a line at a time - output it. */
+  while (fgets(path, sizeof(path), fp) != NULL) {
+//    printf("%s", path);
+  }
+  pclose(fp);
+
+  if(path[0] == '1') {
+//    printf("[pam_sm_acct_mgmt] Headphones connected\n");
+    return PAM_SUCCESS;
+  } else {
+//    printf("[pam_sm_acct_mgmt] Headphones not connected\n");
+    return PAM_AUTH_ERR;
+  }
+}
+
+/*
+ * get_device_id
+ *
+ * Reads the first device identifier from ZIA config file
+ *
+ */
+static void get_device_id(char *user, char *line, int size) {
+  char dev_id_file_path[500];
+  strcpy(dev_id_file_path, "/Users/");
+  strcat(dev_id_file_path, user);
+  strcat(dev_id_file_path, "/.zia/devices");
+  FILE *f = fopen(dev_id_file_path, "r");
+  while(fgets(line, size, f) != NULL) {
+    if(line[strlen(line)-1] == '\n') {
+      line[strlen(line)-1] = '\0';
+    }
+//    printf("Read \"%s\"\n", line);
+    fclose(f);
+    return;
+  }
+}
+
 /* expected hook */
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-	return PAM_SUCCESS;
+    return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("Acct mgmt\n");
-	return PAM_SUCCESS;
+  int retval;
+//  printf("Acct mgmt\n");
+  char devid[64];
+
+  const char* pUsername;
+  retval = pam_get_user(pamh, &pUsername, "Username: ");
+  get_device_id((char*)pUsername, devid, sizeof(devid));
+  
+  retval = check_device_connected(devid);
+
+//  printf("pam_sm_acct_mgmt returning %d\n", retval);
+
+  return retval;
 }
 
 /* expected hook, this is where custom stuff happens */
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-	int retval;
+  int retval;
+  char devid[64];
 
-	const char* pUsername;
-	retval = pam_get_user(pamh, &pUsername, "Username: ");
+  const char* pUsername;
+  retval = pam_get_user(pamh, &pUsername, "Username: ");
 
-	printf("Welcome %s\n", pUsername);
+//  printf("Welcome %s\n", pUsername);
 
-	if (retval != PAM_SUCCESS) {
-		return retval;
-	}
+  if (retval != PAM_SUCCESS) {
+      return retval;
+  }
 
-    if(access("/tmp/zia.d", 0600) ) {
-    }
+  get_device_id((char*)pUsername, devid, sizeof(devid));
+  
+  retval = check_device_connected(devid);
 
-    perror("access");
+//  printf("pam_sm_authenticate returning %d\n", retval);
 
-	if (strcmp(pUsername, "neil") != 0) {
-		return PAM_AUTH_ERR;
-	}
-
-	return PAM_SUCCESS;
+  return retval;
 }
